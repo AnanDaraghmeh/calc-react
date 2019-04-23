feb73ddb4162b5b2d2e8b77642cb69152d7992f7
AnanDaraghmeh <anan.daraghmeh@yahoo.com>
2019-04-23 14:13:24 +0200

    change colors

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Calculator from './Calculator';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Calculator />
+      <div className="app">
+        <div className="container">
+          <Calculator />
+        </div>
       </div>
     );
   }
--- src/Calculator.js
+++ src/Calculator.js
@@ -75,18 +75,18 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div className="d-flex justify-content-center align-items-center my-1">
-        <main className="col-12 p-1 col-sm-10 col-md-8 col-lg-6 col-xl-4 bg-dark">
+      <div className="row justify-content-center align-items-center">
+        <main className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
           <section>
             <div className="input-group mb-1">
               <textarea
                 value={this.state.display}
-                className="form-control rounded-0 mx-auto py-4"
+                className="form-control mx-auto py-4"
                 cols="2"
                 disabled
               />
               <div onClick={this.onDeleteClick} className="input-group-append">
-                <span className="input-group-text rounded-0 del-key">
+                <span className="input-group-text del-key">
                   <FontAwesomeIcon icon={faBackspace} />
                 </span>
               </div>
@@ -94,141 +94,78 @@ class Calculator extends Component {
           </section>
 
           <section className="grid">
-            <button
-              onClick={this.memoryStore}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.memoryStore} className="btn btn-warning">
               MS
             </button>
-            <button
-              onClick={this.memoryRecall}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.memoryRecall} className="btn btn-warning">
               MR
             </button>
-            <button
-              onClick={this.memoryCancel}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.memoryCancel} className="btn btn-warning">
               MC
             </button>
             <button
               onClick={this.onSquareRootClick}
-              className="btn py-2 rounded-0 btn-warning"
+              className="btn btn-warning"
             >
               √
             </button>
-            <button
-              onClick={this.onResetClick}
-              className="btn py-2 rounded-0 btn-danger"
-            >
+            <button onClick={this.onResetClick} className="btn btn-danger">
               AC
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.onNumClick} className="btn btn-warning">
               (
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.onNumClick} className="btn btn-warning">
               )
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.onNumClick} className="btn btn-warning">
               ÷
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               7
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               8
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               9
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.onNumClick} className="btn btn-warning">
               x
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               4
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               5
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               6
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.onNumClick} className="btn btn-warning">
               -
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               1
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               2
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               3
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-warning"
-            >
+            <button onClick={this.onNumClick} className="btn btn-warning">
               +
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               .
             </button>
-            <button
-              onClick={this.onNumClick}
-              className="btn py-2 rounded-0 btn-info"
-            >
+            <button onClick={this.onNumClick} className="btn btn-info">
               0
             </button>
             <button
               onClick={this.onEqualClick}
-              className="btn py-2 rounded-0 btn-primary equal-key"
+              className="btn btn-primary equal-key"
             >
               =
             </button>
